529d03a5bae08380e24dd49188a1b13851e6652e
amramer45 <69049940+amramer45@users.noreply.github.com>
2021-03-22 08:58:30 -0400

    map layers

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiYW1yYW1lcjQ1IiwiYSI6ImNrbTVtdWc0YTBmdDQyb3MzcmRwazJkNGUifQ.gctm_3soNpCCqneLlFjShA";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,39 @@
+//query URL for past 7 days of earthquakes
+var queryURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+
+function createMap(earthquakeData) {
+    //create tile layer that will be the background of our map
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "light-v10",
+        accessToken: API_KEY
+    });
+
+    //create a basemaps object to hold the lightmap layer
+    var baseMaps = {
+        "Light Map": lightmap
+    };
+
+    //create an overlaymaps object to hold the earthquake layer
+    var overlayMaps = {
+        "Earthquakes": earthquakes,
+        "Magnitude": magnitude
+    };
+
+    //create the map object with options
+    var map = L.map("map-id", {
+        center: [40.73, -74.0059],
+        zoom: 3,
+        layers: [lightmap, earthquakes]
+    });
+
+    //create a layer control, pass in basemaps and overlaymaps
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(map);
+}
+
+function createMarkers(response) {
+    
+}
