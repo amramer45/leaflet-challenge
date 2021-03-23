1ea5e4fb0dd785c58574f321beb7c5e3f4be4b38
amramer45 <69049940+amramer45@users.noreply.github.com>
2021-03-23 13:28:07 -0400

    radius

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -48,6 +48,93 @@ function createMap(earthquakeData) {
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(map);
+
+    //Get color radius call to the query URL
+    d3.json(queryURL, function(data) {
+        function styleInfo(feature) {
+            return {
+                opacity: 1,
+                fillOpacity: 1
+                fillColor: getColor(feature.properties.mag),
+                color: "#000",
+                radius: getRadius(feature.properties.mag),
+                stroke: true,
+                weight: 0.5
+            };
+        }
+
+        //Set different colors from magnitude
+        function getColor(magnitude) {
+            switch (true) {
+                case magnitude > 5:
+                    return "#ea2c2c";
+                case magnitude > 4:
+                    return "#ea822c";
+                case magnitude > 3:
+                    return "#ee9c00";
+                case magnitude > 2:
+                    return "#eecc00";
+                case magnitude > 1:
+                    return "#d4ee00";
+                default:
+                    return "#98ee00";
+            }
+        }
+
+        //Set radius from magnitude
+        function getRadius(magnitude) {
+            if (magnitude === 0) {
+                return 1;
+            }
+
+            return magnitude * 4;
+        }
+
+        //GeoJSON layer
+        L.geoJson(data, {
+            //Make circles
+            pointToLayer: function(feature, latlng) {
+                return L.circleMarker(latlng);
+            },
+            //circle style
+            style: styleInfo,
+            //popup
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+            }
+        }).addTo(myMap);
+    })
+
+    //Create legend variable
+    var legend = L.control({
+        position: "bottomright"
+    });
+
+    //details for the legend
+    legend.onAdd = function () {
+        var div = L.DomUtil.create("div", "info legend");
+
+        var grades = [0, 1, 2, 3, 4, 5];
+        var colors = [
+            "#98ee00",
+            "#d4ee00",
+            "#eecc00",
+            "#ee9c00",
+            "#ea822c",
+            "#ea2c2c"
+        ];
+
+        //Loop
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                "<i style = 'background: " colors[i] + " '></i> " +
+                grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+        }
+        return div;
+    };
+
+    //Add legend to the map
+    legend.addTo(map);
 }
 
 function createMarkers(response) {
